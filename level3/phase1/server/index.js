a26db44997b0271ebdfe0c05f51709b0dd09af07
Atul5486 <atulkoushal5486@gmail.com>
2026-06-17 20:15:20 +0530

    Load balancer

---
+++ level3/phase1/server/index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    return res.status(200).json({ message: `hello from ${process.env.SERVER_NAME}` })
+})
+
+
+app.listen(port, () => {
+    console.log(`server started ${port}`)
+})
